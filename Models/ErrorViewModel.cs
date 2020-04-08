6cc1b64c6ff870c2d5a6333c439ca5ff2b261dfd
Howard Edidin <hse@msn.com>
2020-04-08 14:05:12 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vivek.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
